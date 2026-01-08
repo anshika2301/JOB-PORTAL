8b3deea6bd1a8ce73438a6232bfb9c039c9e9de1
Anshika <vermaavani92@gmail.com>
2026-01-08 19:21:56 +0530

    Project Completed

--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -162,8 +162,3 @@ const ApplyJob = () => {
 export default ApplyJob
 
 
-// const data = jobs.filter(job => job._id === id)
-//        if(data.length !== 0){
-//         setJobData(data[0])
-//         console.log(data[0])
-//        }
